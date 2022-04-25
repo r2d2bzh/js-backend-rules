804e4a3a0d0cf45fbba4056a8ea0375df5dce1e2
gautaz <11046232+gautaz@users.noreply.github.com>
2022-04-25 17:39:47 +0200

    bump deps

--- versions.js
+++ versions.js
@@ -3,17 +3,17 @@
 export const nodejs = '>=16.0.0';
 
 export const docker = {
-  dockerBuildNodeJS: '2.0.0',
-  nats: '2.7.4-alpine3.15',
+  dockerBuildNodeJS: '2.0.1',
+  nats: '2.8.1-alpine3.15',
 };
 
 export const npm = {
   '@r2d2bzh/moleculer-healthcheck-middleware': '^1.0.2',
   '@r2d2bzh/moleculer-start-checker-middleware': '^1.0.0',
   '@r2d2bzh/yac': '^1.0.0',
-  ava: '^4.1.0',
-  c8: '^7.11.0',
-  moleculer: '^0.14.19',
+  ava: '^4.2.0',
+  c8: '^7.11.2',
+  moleculer: '^0.14.20',
   nats: '^2.6.1',
   nodemon: '^2.0.15',
   uuid: '^8.3.2',
