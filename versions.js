9898d310862643c7e481d5334459f561c578924d
gautaz <11046232+gautaz@users.noreply.github.com>
2022-05-24 09:32:37 +0200

    bump dependencies

--- versions.js
+++ versions.js
@@ -3,8 +3,8 @@
 export const nodejs = '>=16.0.0';
 
 export const docker = {
-  dockerBuildNodeJS: '2.0.1',
-  nats: '2.8.1-alpine3.15',
+  dockerBuildNodeJS: '2.0.2',
+  nats: '2.8.3-alpine3.15',
 };
 
 export const npm = {
@@ -12,9 +12,9 @@ export const npm = {
   '@r2d2bzh/moleculer-start-checker-middleware': '^1.0.1',
   '@r2d2bzh/yac': '^1.0.1',
   ava: '^4.2.0',
-  c8: '^7.11.2',
+  c8: '^7.11.3',
   moleculer: '^0.14.21',
-  nats: '^2.6.1',
+  nats: '^2.7.0',
   nodemon: '^2.0.16',
   uuid: '^8.3.2',
 };
